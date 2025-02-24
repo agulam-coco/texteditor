8e43b69aeedc0d09ca8982ac513161cbd2666f5e
Jason Chigabatia <aguchija@gmail.com>
2025-02-23 23:53:27 -0600

    Revert "Formatted everything online using style formatter"
    This reverts commit 483786749496374137526ef13d911f0b1ccdfdcf.
    
    REvert all changes by online formatter

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -5,99 +5,99 @@ package edu.grinnell.csc207.texteditor;
  */
 public class SimpleStringBuffer {
 
-  private String stringBuffer;
-  private int sz = 0;
+    private String stringBuffer;
+    private int sz = 0;
 
-  public SimpleStringBuffer() {
-    stringBuffer = "";
-    sz = 0;
-  }
-
-  /**
-   * This function inserts the character ch into the current gap buffer
-   *
-   * @param ch
-   */
-  public void insert(char ch) {
-    String left_portion = stringBuffer.substring(0, sz);
-    String right_portion = stringBuffer.substring(sz);
-    stringBuffer = left_portion + ch + right_portion;
-    sz++;
-    //throw new UnsupportedOperationException("Unimplemented method 'delete'");
-  }
-
-  /**
-   * THis function removes the element at the cursor by adding the string
-   * before and after it
-   */
-  //Credit: https://stackoverflow.com/a/13386147
-  public void delete() {
-    if (sz != 0) {
-      stringBuffer = stringBuffer.substring(0, sz - 1) + stringBuffer.substring(sz);
-      sz--;
+    public SimpleStringBuffer() {
+        stringBuffer = "";
+        sz = 0;
     }
-    //throw new UnsupportedOperationException("Unimplemented method 'delete'");
-  }
 
-  /**
-   * This function returns the current position of the cursor
-   *
-   * @return
-   */
-  public int getCursorPosition() {
-    return sz;
-    //throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
-  }
-
-  /**
-   * This function moves the cursor left by causing
-   */
-  public void moveLeft() {
-    if (sz > 0) {
-      sz--;
+    /**
+     * This function inserts the character ch into the current gap buffer
+     *
+     * @param ch
+     */
+    public void insert(char ch) {
+        String left_portion = stringBuffer.substring(0, sz);
+        String right_portion = stringBuffer.substring(sz);
+        stringBuffer = left_portion + ch + right_portion;
+        sz++;
+        //throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
-    //throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
-  }
 
-  /**
-   * THis function moves the cursor right
-   */
-  public void moveRight() {
-    if (sz < stringBuffer.length()) {
-      sz++;
+    /**
+     * THis function removes the element at the cursor by adding the string
+     * before and after it
+     */
+    //Credit: https://stackoverflow.com/a/13386147
+    public void delete() {
+        if (sz != 0) {
+            stringBuffer = stringBuffer.substring(0, sz - 1) + stringBuffer.substring(sz);
+            sz--;
+        }
+        //throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
-    //throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
-  }
 
-  /**
-   * This function returns the size of the current function
-   *
-   * @return
-   */
-  public int getSize() {
-    return stringBuffer.length();
-    //  throw new UnsupportedOperationException("Unimplemented method 'getSize'");
-  }
+    /**
+     * This function returns the current position of the cursor
+     *
+     * @return
+     */
+    public int getCursorPosition() {
+        return sz;
+        //throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+    }
 
-  /**
-   * This function returns a particular index of a function i
-   *
-   * @param i
-   * @return
-   */
-  public char getChar(int i) throws IndexOutOfBoundsException {
-    return stringBuffer.charAt(i);
-    //throw new UnsupportedOperationException("Unimplemented method 'getChar'");
-  }
+    /**
+     * This function moves the cursor left by causing
+     */
+    public void moveLeft() {
+        if (sz > 0) {
+            sz--;
+        }
+        //throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+    }
 
-  /**
-   * This function returns a string version of buffer
-   *
-   * @return
-   */
-  @Override
-  public String toString() {
-    return stringBuffer;
-    //throw new UnsupportedOperationException("Unimplemented method 'toString'");
-  }
+    /**
+     * THis function moves the cursor right
+     */
+    public void moveRight() {
+        if (sz < stringBuffer.length()) {
+            sz++;
+        }
+        //throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+    }
+
+    /**
+     * This function returns the size of the current function
+     *
+     * @return
+     */
+    public int getSize() {
+        return stringBuffer.length();
+        //  throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+    }
+
+    /**
+     * This function returns a particular index of a function i
+     *
+     * @param i
+     * @return
+     */
+    public char getChar(int i) throws IndexOutOfBoundsException {
+        return stringBuffer.charAt(i);
+        //throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+    }
+
+    /**
+     * This function returns a string version of buffer
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return stringBuffer;
+        //throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
 }
--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -8,272 +8,272 @@ import net.jqwik.api.constraints.*;
 
 public class GapBufferTests {
 
-  /**
-   * TODO: fill me in with unit and property tests!
-   */
-  @Test
-  public void emptyBufferTests() {
+    /**
+     * TODO: fill me in with unit and property tests!
+     */
+    @Test
+    public void emptyBufferTests() {
 
-    GapBuffer temp = new GapBuffer();
-    assertEquals(0, temp.getSize(), "Size is not 0 yo");
+        GapBuffer temp = new GapBuffer();
+        assertEquals(0, temp.getSize(), "Size is not 0 yo");
 
-    //delete test empty
-    temp.delete();
-    assertEquals(0, temp.getSize(), "Size is not 0");
+        //delete test empty
+        temp.delete();
+        assertEquals(0, temp.getSize(), "Size is not 0");
 
-    //move cursor
-    assertEquals(0, temp.getCursorPosition(), "Current Position is not 0");
-    temp.moveRight();
-    assertEquals(0, temp.getCursorPosition(), "After Move Right, Position is not 0");
+        //move cursor
+        assertEquals(0, temp.getCursorPosition(), "Current Position is not 0");
+        temp.moveRight();
+        assertEquals(0, temp.getCursorPosition(), "After Move Right, Position is not 0");
 
-    temp.moveLeft();
-    assertEquals(0, temp.getCursorPosition(), "After Move Left, Position is not 0");
+        temp.moveLeft();
+        assertEquals(0, temp.getCursorPosition(), "After Move Left, Position is not 0");
 
-  }
-
-  @Test
-  public void insertTest() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
-
-    assertEquals("hello world", temp.toString(), "Insertion failed");
-  }
-
-  @Test
-  public void deleteTest() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
-
-    //delete 4 characters
-    for (int i = 0; i < 4; i++) {
-      temp.delete();
     }
 
-    assertEquals("hello w", temp.toString(), "Deletion failed");
-  }
+    @Test
+    public void insertTest() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-  @Test
-  public void deleteTestEdge() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
-
-    //delete 4 characters
-    for (int i = 0; i < 8; i++) {
-      temp.delete();
+        assertEquals("hello world", temp.toString(), "Insertion failed");
     }
 
-    assertEquals(3, temp.getCursorPosition(), "Delete edge case failed");
-  }
+    @Test
+    public void deleteTest() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-  @Test
-  public void cursorPostionTest() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
+        //delete 4 characters
+        for (int i = 0; i < 4; i++) {
+            temp.delete();
+        }
 
-    assertEquals(11, temp.getCursorPosition(), "Get Cursor Failed");
-
-  }
-
-  @Test
-  public void moveLeftTest() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
-
-    //save current position
-    int currentPosition = temp.getCursorPosition();
-
-    //move left four characters
-    for (int i = 0; i < 4; i++) {
-      temp.moveLeft();
+        assertEquals("hello w", temp.toString(), "Deletion failed");
     }
 
-    assertEquals(currentPosition - 4, temp.getCursorPosition(), "Move left failed");
-  }
+    @Test
+    public void deleteTestEdge() {
+        GapBuffer temp = new GapBuffer();
+       temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-  @Test
-  public void moveLeftTestEdge() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
+        //delete 4 characters
+        for (int i = 0; i < 8; i++) {
+            temp.delete();
+        }
 
-    //move left 10 times characters
-    for (int i = 0; i < 10; i++) {
-      temp.moveLeft();
+        assertEquals(3, temp.getCursorPosition(), "Delete edge case failed");
     }
 
-    assertEquals(1, temp.getCursorPosition(), "Move left failed");
-  }
+    @Test
+    public void cursorPostionTest() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-  @Test
-  public void moveRightTest() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
+        assertEquals(11, temp.getCursorPosition(), "Get Cursor Failed");
 
-    //save current position
-    int currentPosition = temp.getCursorPosition();
-
-    //move left four characters
-    for (int i = 0; i < 4; i++) {
-      temp.moveLeft();
-    }
-    //move right four characters
-    for (int i = 0; i < 4; i++) {
-      temp.moveRight();
     }
 
-    assertEquals(currentPosition, temp.getCursorPosition(), "Move right failed");
-  }
+    @Test
+    public void moveLeftTest() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-  @Test
-  public void moveRightTestEdge() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
+        //save current position
+        int currentPosition = temp.getCursorPosition();
 
-    //save current position
-    int currentPosition = temp.getCursorPosition();
+        //move left four characters
+        for (int i = 0; i < 4; i++) {
+            temp.moveLeft();
+        }
 
-    //move left four characters
-    for (int i = 0; i < 4; i++) {
-      temp.moveLeft();
-    }
-    //move right 10 characters
-    for (int i = 0; i < 10; i++) {
-      temp.moveRight();
+        assertEquals(currentPosition - 4, temp.getCursorPosition(), "Move left failed");
     }
 
-    assertEquals(currentPosition, temp.getCursorPosition(), "Move right failed");
-  }
+    @Test
+    public void moveLeftTestEdge() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-  @Test
-  public void testGetChar() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
+        //move left 10 times characters
+        for (int i = 0; i < 10; i++) {
+            temp.moveLeft();
+        }
 
-    assertEquals(' ', temp.getChar(5), "Get Char Failed");
-
-  }
-
-  @Test
-  public void testGetCharEdge() {
-    GapBuffer temp = new GapBuffer();
-    temp.insert('h');
-    temp.insert('e');
-    temp.insert('l');
-    temp.insert('l');
-    temp.insert('o');
-    temp.insert(' ');
-    temp.insert('w');
-    temp.insert('o');
-    temp.insert('r');
-    temp.insert('l');
-    temp.insert('d');
-
-    try {
-      temp.getChar(11);
-      fail("Did not throw Index out of Bounds Error");
-
-    } catch (IndexOutOfBoundsException e) {
-      //program did exactly what was expected
-      assertTrue(true);
+        assertEquals(1, temp.getCursorPosition(), "Move left failed");
     }
-  }
 
-  @Property
-  boolean sizePropertyTest(@ForAll @IntRange(min = 1, max = 1000) int sz) {
-    GapBuffer temp = new GapBuffer();
+    @Test
+    public void moveRightTest() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
 
-    for (int i = 0; i < sz; i++) {
-      temp.insert('a');
+        //save current position
+        int currentPosition = temp.getCursorPosition();
+
+        //move left four characters
+        for (int i = 0; i < 4; i++) {
+            temp.moveLeft();
+        }
+        //move right four characters
+        for (int i = 0; i < 4; i++) {
+            temp.moveRight();
+        }
+
+        assertEquals(currentPosition, temp.getCursorPosition(), "Move right failed");
     }
-    return temp.getSize() == sz;
-  }
+
+    @Test
+    public void moveRightTestEdge() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
+
+        //save current position
+        int currentPosition = temp.getCursorPosition();
+
+        //move left four characters
+        for (int i = 0; i < 4; i++) {
+            temp.moveLeft();
+        }
+        //move right 10 characters
+        for (int i = 0; i < 10; i++) {
+            temp.moveRight();
+        }
+
+        assertEquals(currentPosition, temp.getCursorPosition(), "Move right failed");
+    }
+
+    @Test
+    public void testGetChar() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
+
+        assertEquals(' ', temp.getChar(5), "Get Char Failed");
+
+    }
+
+    @Test
+    public void testGetCharEdge() {
+        GapBuffer temp = new GapBuffer();
+        temp.insert('h');
+        temp.insert('e');
+        temp.insert('l');
+        temp.insert('l');
+        temp.insert('o');
+        temp.insert(' ');
+        temp.insert('w');
+        temp.insert('o');
+        temp.insert('r');
+        temp.insert('l');
+        temp.insert('d');
+
+        try {
+            temp.getChar(11);
+            fail("Did not throw Index out of Bounds Error");
+
+        } catch (IndexOutOfBoundsException e) {
+            //program did exactly what was expected
+            assertTrue(true);
+        }
+    }
+
+    @Property
+    boolean sizePropertyTest(@ForAll @IntRange(min = 1, max = 1000) int sz) {
+        GapBuffer temp = new GapBuffer();
+
+        for (int i = 0; i < sz; i++) {
+            temp.insert('a');
+        }
+        return temp.getSize() == sz;
+    }
 }
