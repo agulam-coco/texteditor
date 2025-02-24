483786749496374137526ef13d911f0b1ccdfdcf
Jason Chigabatia <aguchija@gmail.com>
2025-02-23 23:47:41 -0600

    Formatted everything online using style formatter

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -7,162 +7,162 @@ import java.util.Arrays;
  */
 public class GapBuffer {
 
-    final int INITIAL_BUFFER_SIZE = 10;
-    char[] buffer = new char[INITIAL_BUFFER_SIZE];
+  final int INITIAL_BUFFER_SIZE = 10;
+  char[] buffer = new char[INITIAL_BUFFER_SIZE];
 
-    private int gapStartingIndex;
-    private int afterCursorIndex;
+  private int gapStartingIndex;
+  private int afterCursorIndex;
 
-    public GapBuffer() {
-        gapStartingIndex = 0;
-        afterCursorIndex = INITIAL_BUFFER_SIZE;
+  public GapBuffer() {
+    gapStartingIndex = 0;
+    afterCursorIndex = INITIAL_BUFFER_SIZE;
+  }
+
+  /**
+   * This function expands a Gap Buffer aby doubling its internal size
+   */
+  private void expand() {
+
+    //new array of double size 
+    char[] newArray = new char[buffer.length * 2];
+
+    //copy portion before gap
+    System.arraycopy(buffer, 0, newArray, 0, gapStartingIndex);
+
+    int numberAfterGap = buffer.length - afterCursorIndex;
+
+    int newAfterCursorIndex = newArray.length - numberAfterGap;
+
+    //copy portion after gap 
+    if (numberAfterGap > 0) {
+      System.arraycopy(buffer, afterCursorIndex, newArray, newAfterCursorIndex - 1, numberAfterGap);
     }
 
-    /**
-     * This function expands a Gap Buffer aby doubling its internal size
-     */
-    private void expand() {
+    //assign new array
+    buffer = newArray;
 
-        //new array of double size 
-        char[] newArray = new char[buffer.length * 2];
+    //move after cursor to new 
+    afterCursorIndex = newAfterCursorIndex;
 
-        //copy portion before gap
-        System.arraycopy(buffer, 0, newArray, 0, gapStartingIndex);
+  }
 
-        int numberAfterGap = buffer.length - afterCursorIndex;
+  /**
+   * This function inserts the character ch into the current gap buffer
+   *
+   * @param ch
+   */
+  public void insert(char ch) {
 
-        int newAfterCursorIndex = newArray.length - numberAfterGap;
+    buffer[gapStartingIndex] = ch;
+    gapStartingIndex++;
 
-        //copy portion after gap 
-        if (numberAfterGap > 0) {
-            System.arraycopy(buffer, afterCursorIndex, newArray, newAfterCursorIndex - 1, numberAfterGap);
-        }
+    if (gapStartingIndex == afterCursorIndex) {
+      //expand
+      expand();
+    }
+    //throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  }
 
-        //assign new array
-        buffer = newArray;
+  /**
+   * THis function removes the element behind the current gap by pointing the gap to it
+   */
+  public void delete() {
+    //decrement gap starting index unless it is at the start of array
+    if (gapStartingIndex != 0) {
+      gapStartingIndex--;
+    }
+    //throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  }
 
-        //move after cursor to new 
-        afterCursorIndex = newAfterCursorIndex;
-        
+  /**
+   * This function returns the current position of the cursor
+   * @return 
+   */
+  public int getCursorPosition() {
+    return gapStartingIndex;
+  }
+
+  /**
+   * This function moves the cursor left by causing 
+   */
+  public void moveLeft() {
+    //if gapStaringIndex > 0
+    //move gap starting index back one.
+    //move after cursor index back one 
+    //take whatever is at gapStartingIndex position and move it to afterCursor position
+
+    if (gapStartingIndex > 0) {
+      gapStartingIndex--;
+      afterCursorIndex--;
+      buffer[afterCursorIndex] = buffer[gapStartingIndex];
+    }
+  }
+
+  /**
+   * THis function moves the cursor right
+   */
+  public void moveRight() {
+    //if aftercursor < buffer length
+    //copy value in after cursor to gapStartingBuffer.
+    //increment gapStartingIndex
+    //increment afterCursorIndex
+    if (afterCursorIndex < buffer.length) {
+      buffer[gapStartingIndex] = buffer[afterCursorIndex];
+      gapStartingIndex++;
+      afterCursorIndex++;
+    }
+  }
+
+  /**
+   * This function returns the size of the current function
+   * @return 
+   */
+  public int getSize() {
+    //buffer length - length of gap +1 <-- for shift by index
+
+    if (gapStartingIndex == 0 && afterCursorIndex == buffer.length) {
+      return 0;
+    }
+    return (buffer.length - (afterCursorIndex - gapStartingIndex));
+  }
+
+  /** This function returns a particular index of a function i
+   * @param i
+   * @return 
+   */
+  public char getChar(int i) {
+    //in the part after gap
+    if (i >= gapStartingIndex) {
+      return buffer[afterCursorIndex + (i - gapStartingIndex)];
+    } //in the part before gap
+    else {
+      return buffer[i];
+    }
+    //throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+  }
+
+  /**
+   * This function returns a string version of buffer
+   * @return 
+   */
+  //credit: https://stackoverflow.com/a/4822280
+  public String toString() {
+    //add first part before gap to part after gap
+    String firstHalf;
+    String secondHalf;
+
+    if (gapStartingIndex == 0) {
+      firstHalf = "";
+    } else {
+      firstHalf = String.valueOf(Arrays.copyOfRange(buffer, 0, gapStartingIndex));
     }
 
-    /**
-     * This function inserts the character ch into the current gap buffer
-     *
-     * @param ch
-     */
-    public void insert(char ch) {
-
-        buffer[gapStartingIndex] = ch;
-        gapStartingIndex++;
-
-        if (gapStartingIndex == afterCursorIndex) {
-            //expand
-            expand();
-        }
-        //throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    if (afterCursorIndex == buffer.length) {
+      secondHalf = "";
+    } else {
+      secondHalf = String.valueOf(Arrays.copyOfRange(buffer, afterCursorIndex, buffer.length));
     }
 
-    /**
-     * THis function removes the element behind the current gap by pointing the gap to it
-     */
-    public void delete() {
-        //decrement gap starting index unless it is at the start of array
-        if (gapStartingIndex != 0) {
-            gapStartingIndex--;
-        }
-        //throw new UnsupportedOperationException("Unimplemented method 'delete'");
-    }
-
-    /**
-     * This function returns the current position of the cursor
-     * @return 
-     */
-    public int getCursorPosition() {
-        return gapStartingIndex;
-    }
-
-    /**
-     * This function moves the cursor left by causing 
-     */
-    public void moveLeft() {
-        //if gapStaringIndex > 0
-        //move gap starting index back one.
-        //move after cursor index back one 
-        //take whatever is at gapStartingIndex position and move it to afterCursor position
-
-        if (gapStartingIndex > 0) {
-            gapStartingIndex--;
-            afterCursorIndex--;
-            buffer[afterCursorIndex] = buffer[gapStartingIndex];
-        }
-    }
-
-    /**
-     * THis function moves the cursor right
-     */
-    public void moveRight() {
-        //if aftercursor < buffer length
-        //copy value in after cursor to gapStartingBuffer.
-        //increment gapStartingIndex
-        //increment afterCursorIndex
-        if (afterCursorIndex < buffer.length) {
-            buffer[gapStartingIndex] = buffer[afterCursorIndex];
-            gapStartingIndex++;
-            afterCursorIndex++;
-        }
-    }
-    
-    /**
-     * This function returns the size of the current function
-     * @return 
-     */
-    public int getSize() {
-        //buffer length - length of gap +1 <-- for shift by index
-        
-        if(gapStartingIndex == 0 && afterCursorIndex == buffer.length){
-            return 0;
-        }
-        return (buffer.length - (afterCursorIndex - gapStartingIndex ));
-    }
-
-    /** This function returns a particular index of a function i
-     * @param i
-     * @return 
-     */
-    public char getChar(int i) {
-        //in the part after gap
-        if (i >= gapStartingIndex) {
-            return buffer[afterCursorIndex + (i - gapStartingIndex)];
-        } //in the part before gap
-        else {
-            return buffer[i];
-        }
-        //throw new UnsupportedOperationException("Unimplemented method 'getChar'");
-    }
-
-    /**
-     * This function returns a string version of buffer
-     * @return 
-     */
-    //credit: https://stackoverflow.com/a/4822280
-    public String toString() {
-        //add first part before gap to part after gap
-        String firstHalf;
-        String secondHalf;
-
-        if (gapStartingIndex == 0) {
-            firstHalf = "";
-        } else {
-            firstHalf = String.valueOf(Arrays.copyOfRange(buffer, 0, gapStartingIndex));
-        }
-
-        if (afterCursorIndex == buffer.length) {
-            secondHalf = "";
-        } else {
-            secondHalf = String.valueOf(Arrays.copyOfRange(buffer, afterCursorIndex, buffer.length));
-        }
-
-        return firstHalf + secondHalf;
-    }
+    return firstHalf + secondHalf;
+  }
 }
--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -16,108 +16,107 @@ import java.nio.file.Paths;
  */
 public class TextEditor {
 
-    /**
-     * The main entry point for the TextEditor application.
-     *
-     * @param args command-line arguments.
-     */
-    public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
-            printUsage();
-            System.exit(1);
+  /**
+   * The main entry point for the TextEditor application.
+   *
+   * @param args command-line arguments.
+   */
+  public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      printUsage();
+      System.exit(1);
+    }
+
+    // TODO: fill me in with a text editor TUI!
+    Path path = Paths.get(args[0]);
+    System.out.format("Loading %s...\n", path.getFileName());
+
+    //DECLARE VARIABLES
+    GapBuffer buffer = new GapBuffer();
+    Screen screen = new DefaultTerminalFactory().createScreen();
+    screen.startScreen();
+
+    if (Files.exists(path) && Files.isRegularFile(path)) {
+      String fileContents = Files.readString(path);
+      insertInitial(buffer, fileContents);
+      drawBuffer(buffer, screen);
+    }
+
+    OUTER:
+      while (true) {
+        KeyStroke userInput = screen.readInput();
+        KeyType userKeyType = userInput.getKeyType();
+        switch (userKeyType) {
+        case Character -> {
+          //add character
+          char insertCharacter = userInput.getCharacter();
+          buffer.insert(insertCharacter);
         }
-
-        // TODO: fill me in with a text editor TUI!
-        Path path = Paths.get(args[0]);
-        System.out.format("Loading %s...\n", path.getFileName());
-
-        //DECLARE VARIABLES
-        GapBuffer buffer = new GapBuffer();
-        Screen screen = new DefaultTerminalFactory().createScreen();
-        screen.startScreen();
-
-        if (Files.exists(path) && Files.isRegularFile(path)) {
-            String fileContents = Files.readString(path);
-            insertInitial(buffer, fileContents);
-            drawBuffer(buffer, screen);
+        case ArrowLeft ->
+        buffer.moveLeft();
+        //perform arrow left
+        case ArrowRight ->
+        buffer.moveRight();
+        //perform arrow right
+        case Backspace ->
+        buffer.delete();
+        //perform backspace
+        case Escape -> {
+          break OUTER;
         }
-
-        OUTER:             
-        while (true) {
-            KeyStroke userInput = screen.readInput();
-            KeyType userKeyType = userInput.getKeyType();
-            switch (userKeyType) {
-                case Character -> {
-                    //add character
-                    char insertCharacter = userInput.getCharacter();
-                    buffer.insert(insertCharacter);
-                }
-                case ArrowLeft ->
-                    buffer.moveLeft();
-                //perform arrow left
-                case ArrowRight ->
-                    buffer.moveRight();
-                //perform arrow right
-                case Backspace ->
-                    buffer.delete();
-                //perform backspace
-                case Escape -> {
-                    break OUTER;
-                }
-                default -> {
-                }
-            }
-            drawBuffer(buffer, screen);
+        default -> {}
         }
-        //cleanup
-        screen.stopScreen();
+        drawBuffer(buffer, screen);
+      }
+    //cleanup
+    screen.stopScreen();
 
-        Files.writeString(path, buffer.toString());
+    Files.writeString(path, buffer.toString());
+
+  }
+
+  /**
+   * Helper function to  print usage file
+   */
+  private static void printUsage() {
+    System.err.println("Usage: java TextEditor <filename>");
+  }
+
+  /**
+   * This function draws the buffer passed to it to the screen 
+   * @param buf
+   * @param screen
+   * @throws IOException 
+   */
+  public static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
+    screen.clear();
+    for (int index = 0; index < buf.getSize(); index++) {
+      //set character in terminal
+      int row = index % screen.getTerminalSize().getColumns();
+      int col = index / screen.getTerminalSize().getColumns();
+      screen.setCharacter(row, col, new TextCharacter(buf.getChar(index)));
 
     }
 
-    /**
-     * Helper function to  print usage file
-     */
-    private static void printUsage() {
-        System.err.println("Usage: java TextEditor <filename>");
+    //set cursor
+    int cursorX = buf.getCursorPosition() % screen.getTerminalSize().getColumns();
+    int cursorY = buf.getCursorPosition() / screen.getTerminalSize().getColumns();
+    screen.setCursorPosition(new TerminalPosition(cursorX, cursorY));
+
+    screen.refresh();
+  }
+
+  /**
+   * This function inserts the initial content into the buffer character by character unless its length is 0
+   * @param buf
+   * @param initialContent 
+   */
+  public static void insertInitial(GapBuffer buf, String initialContent) {
+    if (initialContent.length() == 0) {
+      return;
     }
-
-    /**
-     * This function draws the buffer passed to it to the screen 
-     * @param buf
-     * @param screen
-     * @throws IOException 
-     */
-    public static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
-        screen.clear();
-        for (int index = 0; index < buf.getSize(); index++) {
-            //set character in terminal
-            int row = index % screen.getTerminalSize().getColumns();
-            int col = index / screen.getTerminalSize().getColumns();
-            screen.setCharacter(row, col, new TextCharacter(buf.getChar(index)));
-
-        }
-
-        //set cursor
-        int cursorX = buf.getCursorPosition() % screen.getTerminalSize().getColumns();
-        int cursorY = buf.getCursorPosition() / screen.getTerminalSize().getColumns();
-        screen.setCursorPosition(new TerminalPosition(cursorX, cursorY));
-
-        screen.refresh();
+    for (int i = 0; i < initialContent.length(); i++) {
+      buf.insert(initialContent.charAt(i));
     }
-
-    /**
-     * This function inserts the initial content into the buffer character by character unless its length is 0
-     * @param buf
-     * @param initialContent 
-     */
-    public static void insertInitial(GapBuffer buf, String initialContent) {
-        if (initialContent.length() == 0) {
-            return;
-        }
-        for (int i = 0; i < initialContent.length(); i++) {
-            buf.insert(initialContent.charAt(i));
-        }
-    }
+  }
 }
