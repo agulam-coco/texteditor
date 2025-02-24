5262cd0c1368b324cb7480f04a9b3b0facce5ff6
agulam-coco <aguchija@gmail.com>
2025-02-23 18:23:20 -0600

    Implemented terminal

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,5 +1,14 @@
 package edu.grinnell.csc207.texteditor;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.screen.Screen;
+import java.io.IOException;
+
 /**
  * The driver for the TextEditor Application.
  */
@@ -7,30 +16,68 @@ public class TextEditor {
 
     /**
      * The main entry point for the TextEditor application.
+     *
      * @param args command-line arguments.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
 
-        GapBuffer temp = new GapBuffer();
-        temp.insert('h');
-        temp.insert('e');
-        temp.insert('l');
-        temp.insert('l');
-        temp.insert('o');
-        temp.insert(' ');
-        temp.insert('w');
-        temp.insert('o');
-        temp.insert('r');
-        temp.insert('l');
-        temp.insert('d');
-        System.out.println(temp.toString());
-        
         // TODO: fill me in with a text editor TUI!
         String path = args[0];
         System.out.format("Loading %s...\n", path);
+
+        GapBuffer buffer = new GapBuffer();
+        Screen screen = new DefaultTerminalFactory().createScreen();
+
+        //start
+        screen.startScreen();
+
+        //main program loop
+        while (true) {
+            KeyStroke userInput = screen.readInput();
+            KeyType userKeyType = userInput.getKeyType();
+            if (userKeyType.equals(KeyType.Character)) {
+                //add character
+                char insertCharacter = userInput.getCharacter();
+                buffer.insert(insertCharacter);
+            } else if (userKeyType.equals(KeyType.ArrowLeft)) {
+                buffer.moveLeft();
+                //perform arrow left
+            } else if (userKeyType.equals(KeyType.ArrowRight)) {
+                buffer.moveRight();
+                //perform arrow right
+            } else if (userKeyType.equals(KeyType.Backspace)) {
+                buffer.delete();
+                //perform backspace
+            } else if (userKeyType.equals(KeyType.Escape)) {
+                break;
+            }
+            drawBuffer(buffer, screen);
+        }
+        //cleanup
+        screen.stopScreen();
+
+    }
+
+    public static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
+
+        for (int index = 0; index < buf.getSize(); index++) {
+
+            //set character in termainal
+            int row = index / screen.getTerminalSize().getRows();
+            int col = index % screen.getTerminalSize().getColumns();
+            screen.setCharacter(row, col, new TextCharacter(buf.getChar(index)));
+
+        }
+
+        //set cursor
+        int cursorX = buf.getCursorPosition() / screen.getTerminalSize().getRows();
+        int cursorY = buf.getCursorPosition() % screen.getTerminalSize().getColumns();
+        screen.setCursorPosition(new TerminalPosition(cursorX, cursorY));
+
+        screen.refresh();
     }
 }
