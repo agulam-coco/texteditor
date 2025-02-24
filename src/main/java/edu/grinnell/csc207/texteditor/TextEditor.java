8d73f2b9f8e7f67d264c6576170ecf3a6a00e800
Jason Chigabatia <aguchija@gmail.com>
2025-02-23 19:58:26 -0600

    Implemented file savign and read and write ability

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -8,6 +8,10 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * The driver for the TextEditor Application.
@@ -21,62 +25,74 @@ public class TextEditor {
      */
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
-            System.err.println("Usage: java TextEditor <filename>");
+            printUsage();
             System.exit(1);
         }
 
         // TODO: fill me in with a text editor TUI!
-        String path = args[0];
-        System.out.format("Loading %s...\n", path);
+        Path path = Paths.get(args[0]);
+        System.out.format("Loading %s...\n", path.getFileName());
 
+        //DECLARE VARIABLES
         GapBuffer buffer = new GapBuffer();
         Screen screen = new DefaultTerminalFactory().createScreen();
-
-        //start
         screen.startScreen();
 
-        OUTER:
+        if (Files.exists(path) && Files.isRegularFile(path)) {
+            String fileContents = Files.readString(path);
+            insertInitial(buffer, fileContents);
+            drawBuffer(buffer, screen);
+        }
+//  System.out.print("This is not a valid file name!");
+//            printUsage();
+//            System.exit(2);
+//        
+        OUTER:             //start
         while (true) {
             KeyStroke userInput = screen.readInput();
             KeyType userKeyType = userInput.getKeyType();
             switch (userKeyType) {
-                case Character:
+                case Character -> {
                     //add character
                     char insertCharacter = userInput.getCharacter();
                     buffer.insert(insertCharacter);
-                    break;
-                case ArrowLeft:
+                }
+                case ArrowLeft ->
                     buffer.moveLeft();
-                    //perform arrow left
-                    break;
-                case ArrowRight:
+                //perform arrow left
+                case ArrowRight ->
                     buffer.moveRight();
-                    //perform arrow right
-                    break;
-                case Backspace:
+                //perform arrow right
+                case Backspace ->
                     buffer.delete();
-                    //perform backspace
-                    break;
-                case Escape:
+                //perform backspace
+                case Escape -> {
                     break OUTER;
-                default:
-                    break;
+                }
+                default -> {
+                }
             }
             drawBuffer(buffer, screen);
         }
         //cleanup
         screen.stopScreen();
 
+        Files.writeString(path, buffer.toString());
+
+    }
+
+    private static void printUsage() {
+        System.err.println("Usage: java TextEditor <filename>");
     }
 
     public static void drawBuffer(GapBuffer buf, Screen screen) throws IOException {
         screen.clear();
-        for(int index = 0; index < buf.getSize();index++){
+        for (int index = 0; index < buf.getSize(); index++) {
             //set character in terminal
             int row = index % screen.getTerminalSize().getColumns();
             int col = index / screen.getTerminalSize().getColumns();
             screen.setCharacter(row, col, new TextCharacter(buf.getChar(index)));
-           
+
         }
 
         //set cursor
@@ -85,5 +101,14 @@ public class TextEditor {
         screen.setCursorPosition(new TerminalPosition(cursorX, cursorY));
 
         screen.refresh();
+    }
+
+    public static void insertInitial(GapBuffer buf, String initialContent) {
+        if (initialContent.length() == 0) {
+            return;
+        }
+        for (int i = 0; i < initialContent.length(); i++) {
+            buf.insert(initialContent.charAt(i));
+        }
     }
 }
